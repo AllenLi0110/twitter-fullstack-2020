16c8e67be28c103d7c4192201c478cb41ee43504
AllenliIE <allen.li.service@gmail.com>
2023-01-18 22:47:21 +0800

    feat: init twitter

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const helpers = require('./_helpers');
+const express = require("express")
+const routes = require("./routes")
+const helpers = require("./_helpers")
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
+const db = require("./models")
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(routes)
+app.listen(port, () => console.log(`Twitter Fullstack listening on port ${port}!`))
 
 module.exports = app
