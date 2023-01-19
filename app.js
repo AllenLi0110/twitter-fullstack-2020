3ad0841470dbff850eccb7e71bf7fa5990615ab5
AllenliIE <allen.li.service@gmail.com>
2023-01-19 16:25:00 +0800

    feat: add tweet index page

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const routes = require("./routes")
+const handlebars = require("express-handlebars")
+
 const helpers = require("./_helpers")
+// use helpers.getUser(req) to replace req.user
+// use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
 const app = express()
 const port = process.env.PORT || 3000
-
-const db = require("./models")
-// use helpers.getUser(req) to replace req.user
-// use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+app.engine("hbs", handlebars({extname:".hbs"}))
+app.set("view engine", "hbs")
 
 app.use(routes)
 app.listen(port, () => console.log(`Twitter Fullstack listening on port ${port}!`))
