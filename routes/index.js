dc79bf853bcadd3259e708a852762da6de7d4653
AllenliIE <allen.li.service@gmail.com>
2023-01-20 13:11:07 +0800

    feat: add admin index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const router = express.Router()
 
+const admin = require("./modules/admin")
 const tweetController = require("../controllers/tweet-controller")
 
+router.use("/admin", admin)
 router.get("/tweets", tweetController.getTweets)
 router.use("/", (req, res) => res.redirect("/tweets"))
 
