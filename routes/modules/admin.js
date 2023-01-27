5b40d95e846e00fafe3faaad917b5ab73bad0ce5
AllenliIE <allen.li.service@gmail.com>
2023-01-28 02:51:06 +0800

    feat: add auth.js in middlerware

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,8 +2,9 @@ const express = require("express")
 const router = express.Router()
 
 const adminController = require("../../controllers/admin-controller")
+const { authenticatedAdmin } = require("../../middleware/auth")
 
-router.get("/tweets", adminController.getTweets)
+router.get("/tweets", authenticatedAdmin, adminController.getTweets)
 router.use("/", (req, res) => res.redirect("/admin/tweets"))
 
 module.exports = router
