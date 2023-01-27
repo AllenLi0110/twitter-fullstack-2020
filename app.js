1789a6e21806a62fe6963de8d869dd20314062af
AllenliIE <allen.li.service@gmail.com>
2023-01-27 17:11:43 +0800

    feat: add auth-helpers and handlebars-helpers

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const handlebars = require("express-handlebars")
 const flash = require("connect-flash")
 const session = require("express-session")
 const passport = require("./config/passport")
-const helpers = require("./_helpers")
-// use helpers.getUser(req) to replace req.user
-// use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+const helpers = require("./helpers/auth-helpers")
+const handlebarsHelpers = require("./helpers/handlebars-helpers")
 
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = "secret"
 
-app.engine("hbs", handlebars({extname:".hbs"}))
+app.engine("hbs", handlebars({ extname: ".hbs", helpers: handlebarsHelpers }))
 app.set("view engine", "hbs")
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
@@ -23,6 +22,7 @@ app.use(flash())
 app.use((req, res, next) => {
 	res.locals.success_messages = req.flash("success_messages")
 	res.locals.error_messages = req.flash("error_messages")
+	res.locals.user = helpers.getUser(req)
 	next()
 })
 app.use(routes)
