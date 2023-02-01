43fbfa768015e6fc79f6e20a916a3c8c7bc0b101
AllenliIE <allen.li.service@gmail.com>
2023-02-01 21:10:49 +0800

    feat: add pagination on tweets index page

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -1,22 +1,36 @@
 const { Tweet, User, Like, Reply } = require("../models")
+const { getOffset, getPagination } = require("../helpers/pagination-helper")
 
 const tweetController = {
-	getTweets:(req, res) => {
+	getTweets:(req, res, next) => {
+		const DEFAULT_LIMIT = 7
+		const page = Number(req.query.page) || 1
+		const limit = Number(req.query.limit) || DEFAULT_LIMIT
+		const offset = getOffset(limit, page)
+
 		return Tweet.findAll({
 			include: [
 				User,
 				Like,
 				Reply
-			]
+			],
+			order:[
+				["created_at", "DESC"],
+				["id", "ASC"]
+			],
+			limit,
+			offset
 		}).then(tweets => {
 			const data = tweets.map(t => ({
 				...t,
 				description: t.description.substring(0, 140)
 			}))
 			return res.render("tweets", {
-				tweets: data
+				tweets: data,
+				pagination: getPagination(limit, page, tweets.count)
 			})
 		})
+			.catch(err => next(err))
 	}
 }
 
