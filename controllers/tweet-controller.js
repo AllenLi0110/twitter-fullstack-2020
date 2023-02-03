ac486d916e465e3dc02c721e912c834520504454
AllenliIE <allen.li.service@gmail.com>
2023-02-03 10:36:57 +0800

    feat: modify tweet page and change user avatar

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -3,38 +3,38 @@ const { getOffset, getPagination } = require("../helpers/pagination-helper")
 const { getUser } = require("../helpers/auth-helpers")
 
 const tweetController = {
-	getTweets:(req, res, next) => {
-		const DEFAULT_LIMIT = 7
-		const page = Number(req.query.page) || 1
-		const limit = Number(req.query.limit) || DEFAULT_LIMIT
-		const offset = getOffset(limit, page)
+	getTweets: async (req, res, next) => {
+		try {
+			const DEFAULT_LIMIT = 7
+			const page = Number(req.query.page) || 1
+			const limit = Number(req.query.limit) || DEFAULT_LIMIT
+			const offset = getOffset(limit, page)
+			const user = getUser(req)
 
-		return Tweet.findAll({
-			include: [
-				User,
-				Like,
-				Reply
-			],
-			order:[
-				["created_at", "DESC"],
-				["id", "ASC"]
-			],
-			limit,
-			offset
-		}).then(tweets => {
-			const data = tweets.map(t => ({
-				...t,
-				description: t.description.substring(0, 140)
+			const tweetsList = await Tweet.findAll({
+				include: [User, Reply, Like],
+				order: [
+					["created_at", "DESC"],
+					["id", "ASC"],
+				],
+				limit,
+				offset,
+			})
+			const tweets = tweetsList.map((tweet) => ({
+				...tweet.toJSON(),
+				isLiked: tweet.Likes.some((t) => t.UserId === user.id),
 			}))
 			return res.render("tweets", {
-				tweets: data,
-				pagination: getPagination(limit, page, tweets.count)
+				tweets,
+				user,
+				pagination: getPagination(limit, page, tweets.count),
 			})
-		})
-			.catch(err => next(err))
+		} catch (err) {
+			next(err)
+		}
 	},
 	postTweet: (req, res) => {
-		const { description }  = req.body
+		const { description } = req.body
 
 		if (description.trim() === "") {
 			req.flash("error_messages", "推文的內容不可空白")
@@ -43,16 +43,17 @@ const tweetController = {
 			req.flash("error_messages", "推文的字數不能超過140字")
 			return res.rediect("back")
 		}
-		
+
 		Tweet.create({
 			description,
-			UserId: getUser(req).id
-		}).then(() => {
-			req.flash("success_messages", "推文成功！")
-			return res.redirect("tweets")
+			UserId: getUser(req).id,
 		})
-			.catch(err => next(err))
-	}
+			.then(() => {
+				req.flash("success_messages", "推文成功！")
+				return res.redirect("tweets")
+			})
+			.catch((err) => next(err))
+	},
 }
 
 module.exports = tweetController
